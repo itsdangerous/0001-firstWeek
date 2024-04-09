fc5a47c3e3949a2eb27dcfa318be6b25b0fdbd37
itsdangerous <danger121@naver.com>
2024-04-10 01:12:00 +0900

    solve: boj_15681_트리와 쿼리

--- gyu/15681_트리와 쿼리.py
+++ gyu/15681_트리와 쿼리.py
@@ -1,1 +1,33 @@
+import sys
 
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+N, R, Q = map(int, input().split())
+tree = [[] for _ in range(N + 1)]
+subtree_size = [0 for _ in range(N + 1)]
+parent = [0 for _ in range(N + 1)]
+
+
+def dfs(node):
+    subtree_size[node] = 1
+    for next_node in tree[node]:
+        if subtree_size[next_node] == 0:
+            parent[next_node] = node
+            dfs(next_node)
+            subtree_size[node] += subtree_size[next_node]
+
+
+for _ in range(N - 1):
+    u, v = map(int, input().split())
+    tree[u].append(v)
+    tree[v].append(u)
+
+dfs(R)
+
+for _ in range(Q):
+    q = int(input())
+    sys.stdout.write(str(subtree_size[q]) + "\n")
+
+print(parent)
+print(subtree_size)
